679725585ce80dfbb3afa99cc25a2e489f4ba009
Mikey Lepori <mlepori19@gmail.com>
2019-06-30 22:49:58 -0500

    Wrote baseline CNN

---
+++ models/baseline_cnn.py
@@ -0,0 +1,17 @@
+import torch.nn as nn 
+
+
+class BaselineCNN(nn.module):
+    def __init__(self):
+        super(BaselineCNN, self).__init__()
+        kernel_size = 3
+        padding = 1
+        layers = []
+        layers.append(nn.Conv2d(in_channels=1, out_channels=1, kernel_size=kernel_size, padding=padding, bias=False))
+        layers.append(nn.BatchNorm2d(1))
+        layers.append(nn.RelU(inplace=True)
+        self.baseline_cnn = nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.baseline_cnn(x)
+        return out
