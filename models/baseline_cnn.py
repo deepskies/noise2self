980f39b257fc870af3686944b3807697222c78c5
Mikey Lepori <mlepori19@gmail.com>
2019-06-30 23:33:02 -0500

    Import error resolved

--- models/baseline_cnn.py
+++ models/baseline_cnn.py
@@ -1,7 +1,7 @@
 import torch.nn as nn 
 
 
-class BaselineCNN(nn.module):
+class BaselineCNN(nn.Module):
     def __init__(self):
         super(BaselineCNN, self).__init__()
         kernel_size = 3
@@ -9,7 +9,7 @@ class BaselineCNN(nn.module):
         layers = []
         layers.append(nn.Conv2d(in_channels=1, out_channels=1, kernel_size=kernel_size, padding=padding, bias=False))
         layers.append(nn.BatchNorm2d(1))
-        layers.append(nn.RelU(inplace=True))
+        layers.append(nn.ReLU(inplace=True))
         self.baseline_cnn = nn.Sequential(*layers)
 
     def forward(self, x):
